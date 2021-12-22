0338b98af52a0c8d20c43f3ba1b7c7f56370b9c1
Michael Neis <michael.neis12@gmail.com>
2021-12-22 12:22:59 -0600

    commit tree 05abb8abcdb8a84ff92ddf5a82fccc95f5c795a4

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+    skip_before_action :authorize, only: :create 
+
+    def show
+        render json: @current_user, status: :ok
+    end
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :image_url, :bio, :password_confirmation)
+    end
+
+end
